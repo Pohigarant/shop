ed9746b9d25a3fc9a5551961478206fcdb243c3c
user1 <pohigarant@,ail.ru>
2026-04-13 12:31:22 +0500

    Добавление сериализатора и представления для регистрации

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,46 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 from users.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", 'username', "first_name", "last_name", "birth_date",
-                  "email","phone","date_joined", 'password']
-        read_only_fields = ["id","date_joined"]
+                  "email", "phone", "date_joined", 'password']
+        read_only_fields = ["id", "date_joined"]
         extra_kwargs = {
             "password": {
                 "write_only": True, }
         }
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "password", "email", "password2"]
+
+    def validate(self, data):
+        if not (data.get("username") or data.get("email")):
+            raise serializers.ValidationError("Нужно указать username or email")
+
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
+        return data
+
+    def create(self, validated_data):
+
+        if not validated_data.get('username'):
+            validated_data['username'] = validated_data['email']
+        validated_data.pop('password2', None)
+
+        user = User.objects.create_user(**validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets, request
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, request, status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from shop1.permissions import IsOwnerOrAdmin
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, RegisterSerializer
 
 
 # Create your views here.
@@ -19,3 +22,22 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.request.user.is_authenticated:
             return User.objects.filter(pk=self.request.user.pk)
         return User.objects.none()
+
+
+class UserRegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                'user': {'id': user.id, 'username': user.username, 'email': user.email},
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            },
+            status=status.HTTP_201_CREATED,
+        )
