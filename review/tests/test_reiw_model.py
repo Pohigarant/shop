0162aba635dd2ab2ee8d53b1c9ccf85fcec1af1c
user1 <pohigarant@,ail.ru>
2026-08-27 18:48:38 +0300

    добавление тестирование модели review

---
+++ review/tests/test_reiw_model.py
@@ -0,0 +1,31 @@
+import pytest
+from django.db import IntegrityError
+from django.urls import reverse
+
+from cart.models import Cart, CartItem
+from categories.models import Category
+from products.models import Product
+from review.models import Review
+
+
+@pytest.fixture
+def category(db):
+    return Category.objects.create(name="test")
+
+
+@pytest.fixture
+def product(db, category):
+    return Product.objects.create(name="test1", price=100, quantity=10, category=category)
+
+@pytest.fixture
+def review(user,product):
+    return Review.objects.create(user=user,product=product,rating=5,text="Отлично")
+
+@pytest.mark.django_db
+def test_create_review(review,user,product):
+    assert review.user == user
+    assert review.product == product
+    assert review.rating==5
+    assert review.text == "Отлично"
+
+
