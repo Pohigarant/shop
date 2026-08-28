5dc1e9f3dee8d26777af76797524d5d5e44861d8
user1 <pohigarant@,ail.ru>
2026-08-28 10:24:00 +0300

    Доработка product serializers

--- products/tests/test_product_serializer.py
+++ products/tests/test_product_serializer.py
@@ -23,8 +23,17 @@ def product(category):
     return Product.objects.create(name="test", category=category, price=500)
 
 
-def test_product_serializer_validate_price_negativ(category):
-    data = {"name": "test", "category": category.id, "price": -500}
+@pytest.mark.django_db
+def test_product_serializer_validate_price_positiv(category):
+    data = {"name": "test", "price": 500}
+
+    serializer = ProductDetailSerializer(data=data)
+    assert serializer.is_valid() is True
+
+
+@pytest.mark.django_db
+def test_product_serializer_validate_price_negativ():
+    data = {"name": "test", "price": -500}
 
     serializer = ProductDetailSerializer(data=data)
     assert serializer.is_valid() is False
@@ -32,24 +41,74 @@ def test_product_serializer_validate_price_negativ(category):
     assert "не может быть отрицательной" in str(serializer.errors["price"])
 
 
-def test_product_serializer_validate_price_positiv(category):
-    data = {"name": "test", "category": category.id, "price": 500}
-
+@pytest.mark.django_db
+def test_product_serializer_create_product():
+    data = {"name": "test", "price": 500, "quantity": 5}
     serializer = ProductDetailSerializer(data=data)
     assert serializer.is_valid() is True
+    product = serializer.save()
+    assert product.name == "test"
+    assert product.price == 500
+    assert product.quantity == 5
+    assert Product.objects.count() == 1
 
 
-def test_product_reviews_count_empty(product):
-    serializer = ProductDetailSerializer(instance=product)
-    assert serializer.data["reviews_count"] == 0
+@pytest.mark.django_db
+def test_product_serializer_update_product(product):
+    data = {"price":197}
+    serializer = ProductDetailSerializer(product, data=data,partial=True)
+    assert serializer.is_valid() is True
+    product = serializer.save()
+    assert product.name == "test"
+    assert product.price == 197
 
 
-def test_get_reviews_count(product, user):
-    # Создаём два отзыва
-    Review.objects.create(product=product, user=user, rating=5)
-    Review.objects.create(product=product, user=user, rating=4)
-    serializer = ProductDetailSerializer
-    count = serializer.get_reviews_count(product)
-    aver = serializer.get_average_rating(product)
-    assert aver == 4.5
-    assert count == 2
+@pytest.mark.django_db
+def test_serializer_empty_name():
+    data={"name": "", "price": 500}
+    serializer = ProductDetailSerializer(data=data)
+    assert serializer.is_valid() is False
+    assert "name" in serializer.errors
+
+@pytest.mark.django_db
+def test_product_serializer_low_quanty():
+    data = {"name": "test", "price": 500, "quantity": -55}
+    serializer = ProductDetailSerializer(data=data)
+    assert serializer.is_valid() is False
+    assert "quantity" in serializer.errors
+
+
+@pytest.mark.django_db
+def test_product_serializer_read_only():
+    data = {
+        "name": "test",
+        "price": 500,
+        "quantity": 10,
+        "id": 9999,  # read_only
+        "created_at": "2020-01-01T00:00:00Z",  # read_only
+        "updated_at": "2020-01-01T00:00:00Z",  # read_only
+        "reviews_count": 100,  # read_only
+        "average_rating": 9.9,  # read_only
+    }
+    serializer = ProductDetailSerializer(data=data)
+    assert serializer.is_valid() is True
+    product = serializer.save()
+    assert product.name == "test"
+    assert product.id != 9999
+    assert str(product.created_at) != "2020-01-01T00:00:00Z"
+    assert serializer.data.get("reviews_count") != 100
+    assert serializer.data.get("average_rating") != 9.9
+
+
+
+
+# @pytest.mark.django_db
+# def test_get_reviews_count(product, user):
+#     # Создаём два отзыва
+#     Review.objects.create(product=product, user=user, rating=5)
+#     Review.objects.create(product=product, user=user, rating=4)
+#     serializer = ProductDetailSerializer(product)
+#     count = serializer.get_reviews_count(product)
+#     aver = serializer.get_average_rating(product)
+#     assert aver == 4.5
+#     assert count == 2
