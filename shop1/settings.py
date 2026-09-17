10c6d9a6bf1591de9723188501c39b82bd5ecc91
user1 <pohigarant@,ail.ru>
2026-09-17 13:58:21 +0300

    Корректировка что бы работало с redis на локальной машине

--- shop1/settings.py
+++ shop1/settings.py
@@ -222,8 +222,10 @@ SIMPLE_JWT = {
     "REVOKE_TOKEN_CLAIM": "hash_password",
     "CHECK_USER_IS_ACTIVE": True,
 }
-REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
-
+# REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
