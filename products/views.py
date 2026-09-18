9a48469c91bb198dac349ca30a73db05766bb7a9
user1 <pohigarant@,ail.ru>
2026-09-18 09:46:54 +0300

    3.3 Исправление что бы работал вложенный роутер категорий у продуктов

--- products/views.py
+++ products/views.py
@@ -43,8 +43,11 @@ class ProductViewSet(viewsets.ModelViewSet):
         return ProductListSerializer
 
     def get_queryset(self):
-        queryset = Product.objects.select_related("category")
-        return queryset
+        qs = Product.objects.select_related("category")
+        category_pk = self.kwargs.get("category_pk")
+        if category_pk:
+            qs = qs.filter(category__id=category_pk)
+        return qs
 
     @action(detail=False, methods=["get"], permission_classes=[AllowAny])
     def popular(self, request, *args, **kwargs):
