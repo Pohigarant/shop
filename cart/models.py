85257f5f2644cde16cfc3f5af4a6c3eae1e061b0
user1 <pohigarant@,ail.ru>
2026-09-18 11:17:57 +0300

    п.1.7 исправление модели корзина

--- cart/models.py
+++ cart/models.py
@@ -9,7 +9,6 @@ class Cart(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        null=True,
         blank=True,
         related_name="cart",
         verbose_name="Покупатель",
