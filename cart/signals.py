8ba030b3f2f5f7fe24f4176523b2e1bf9f4168f9
user1 <pohigarant@,ail.ru>
2026-09-21 13:37:58 +0300

    Исправление неточностией сигнала модели cart

--- cart/signals.py
+++ cart/signals.py
@@ -1,20 +1,14 @@
 from django.conf import settings
-from django.db.models.signals import post_delete, post_save, pre_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from cart.models import Cart, CartItem
-from products.models import Product
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_cart(sender, instance, created, **kwargs):
     if created:
         Cart.objects.create(user=instance)
-
-
-@receiver(pre_delete, sender=Product)
-def delete_products_from_carts(sender, instance, **kwargs):
-    CartItem.objects.filter(product=instance).delete()
 
 
 @receiver(post_save, sender=CartItem)
